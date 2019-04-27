a8acfb706cb8e9310af11d4cc12cf514509c4312
altius <altius@admin-PC>
2019-04-27 15:44:12 +0530

    Habitos final commited 15:43 PM

---
+++ ERP/src/main/java/Idlidabba/ERP/App.java
@@ -0,0 +1,13 @@
+package Idlidabba.ERP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
